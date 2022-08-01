a1d0d749546c729d193fdecf5641032410511898
ektnzpt1080 <ektnzpt1080@naver.com>
2022-08-01 23:00:37 +0900

    change SelectTile logic

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,14 +8,20 @@ public class Tile : MonoBehaviour
 {
     
     [SerializeField] private GameObject highlight;
-    [SerializeField] private Color baseColor, offsetColor;
+    [SerializeField] private Color baseColor, offsetColor, targetColor;
     [SerializeField] private SpriteRenderer _renderer;
     public Vector2 position { get; set; }
     
+    Color originColor;
 
     // Start is called before the first frame update
     public void Init(bool isOffset){
-        _renderer.color = isOffset ? offsetColor : baseColor;
+        originColor = isOffset ? offsetColor : baseColor;
+        _renderer.color = originColor;
+    }
+
+    public void targetTile(bool active){
+        _renderer.color = active ? targetColor : originColor;
     }
 
     public void Highlight(){
@@ -26,6 +32,6 @@ public class Tile : MonoBehaviour
         highlight.SetActive(true);
         yield return new WaitForSeconds(1.0f);
         highlight.SetActive(false);
-
     }
+    
 }
