3ce44294ce742ba3c5ff70fdfbf8dffb9bc8798e
ektnzpt1080 <ektnzpt1080@naver.com>
2022-08-01 23:53:10 +0900

    MoveButton UI Change

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class BattleManager : MonoBehaviour
@@ -26,20 +27,21 @@ public class BattleManager : MonoBehaviour
     bool isBattle;//battle이 시작되었는지
     bool enemyActing;//enmey가 행동중
     int mana; //마나
+    int movementmana;
     
     Turn turn;
     State state;
     Stage stage;
-    [SerializeField] private Collider2D moveButton;
-    SpriteRenderer moveButtonSprite;
     [SerializeField] private Color clickedColor;
     [SerializeField] private Color nonclickedColor;
     
     [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Button moveButton;
+    [SerializeField] TextMeshProUGUI moveButtonText;
 
     void Awake(){
-        moveButtonSprite = moveButton.GetComponent<SpriteRenderer>();
         mana = 3;
+        movementmana = 1;
     }
     void Update(){
         if(isBattle){
@@ -56,13 +58,12 @@ public class BattleManager : MonoBehaviour
                     break;
                 //카드를 고르거나 move 버튼을 누를 수 있음, 혹은 턴 엔드
                 case State.selectBehaviourState :
-                    ClickMoveButton();
                     GameManager.Instance.CardManager.PickCard();
                     EndPlayerTurn();
                     break;
                 //move 버튼을 눌렀을 시, 움직일 범위를 선택 할 수 있음
                 case State.pickMoveState :
-                    Move();
+                    MoveButton();
                     break;
                 //카드를 고른 후 범위를 선택, 몇몇 카드는 자동으로 생략됨
                 case State.pickCardState :
@@ -84,16 +85,13 @@ public class BattleManager : MonoBehaviour
                     break;
             }
             
-            //MoveButton UI
-            if(state == State.pickMoveState){
-                moveButtonSprite.color = clickedColor;
-            }
-            else{
-                moveButtonSprite.color = nonclickedColor;
-            }
 
             //Mana UI
             text.text = "Mana : " + mana + "/3";
+
+            moveButtonText.text = "Move\nneed " + movementmana +" mana";
+            if(mana < movementmana) moveButton.interactable = false;
+            else moveButton.interactable = true;
         }
     }
 
@@ -160,27 +158,23 @@ public class BattleManager : MonoBehaviour
     }
 
     public void ClickMoveButton(){
-        if (Input.GetMouseButtonDown(0) && mana > 0){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);
-            if(hit.collider == moveButton) {
-                foreach(Vector2 pos in Gridlib.CanReach(stage, stage.player.position, 2)){
-                    stage.tiles[pos].Highlight();
-                }
-                state = State.pickMoveState;
+        if (mana >= movementmana && state == State.selectBehaviourState){
+            foreach(Vector2 pos in Gridlib.CanReach(stage, stage.player.position, 2)){
+                stage.tiles[pos].Highlight();
             }
+            state = State.pickMoveState;
         }
     }
 
-    public void Move(){
+    public void MoveButton(){
         if (Input.GetMouseButtonDown(0)){
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray,Mathf.Infinity);    
             if(hit.collider != null && hit.collider.TryGetComponent<Tile>(out Tile tile)){
                 if(Gridlib.CanReach(stage, stage.player.position, 2).Contains(tile.position)){
-                    stage.player.position = tile.position;
-                    stage.player.transform.position = new Vector3(tile.transform.position.x, tile.transform.position.y, stage.player.transform.position.z);
-                    mana--;
+                    stage.player.Move(tile);
+                    mana -= movementmana;
+                    movementmana++;
                 }
                 state = State.selectBehaviourState;
             }
