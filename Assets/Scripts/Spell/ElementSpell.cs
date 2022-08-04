a65ab5bdc50024e930b5b044297f7e73ebdb6dbc
ektnzpt1080 <ektnzpt1080@naver.com>
2022-08-04 19:38:15 +0900

    korean fonts added

--- Assets/Scripts/Spell/ElementSpell.cs
+++ Assets/Scripts/Spell/ElementSpell.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 //나무장벽 301
 public class WoodShield : Spell
 {
-    int shield;
     public override List<Vector2> PreDecision(){
         return null;
     }
     public override void Decision(Vector2 selectedPos){
         Stage stage = GameManager.Instance.BattleManager.GetStage();        
-        stage.player.shield += shield;
+        stage.player.shield += GetValue();
     }
     public override SpellInfo GetSpellInfo(){
         return GameManager.Instance.CardManager.getSpellInfo(301);
